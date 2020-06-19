ec0c7e49952e1f7f61945dad5529f2790e33e0ee
Charlie <charliedeveloperaccess@gmail.com>
2020-06-19 09:38:12 -0500

    clean up code

--- src/inject/content.js
+++ src/inject/content.js
@@ -1,47 +1,47 @@
-const count = 0
+let count = 0
 const CORRECT_M3U8_VIDEO_URL_REQUEST_INDEX = 3
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   count++
 
   if (count >= CORRECT_M3U8_VIDEO_URL_REQUEST_INDEX) {
-    createDownloadButton(request.url)
+    addDownloadButtonToPage(request.url)
   }
 
-  sendResponse('received')
+  sendResponse(200)
 })
 
-function createDownloadButton(videoLink) {
+function addDownloadButtonToPage(videoLink) {
   const downloadButton = getDownloadButton(videoLink)
   const controlBar = document.querySelector('.control-bar--control-bar--MweER')
-  const dlButton = document.querySelector('#udemy-downloader-btn')
+  const liveDownloadButton = document.querySelector('#udemy-downloader-btn')
 
-  addDownloadButtonToPage(controlBar, dlButton, downloadButton)
-}
-
-function addDownloadButtonToPage(controlBar, dlButton, downloadButton) {
   if (!document.body.contains(controlBar)) {
-    let waitForControlBar = setInterval(() => {
-      if (document.body.contains(controlBar) && !document.body.contains(dlButton)) {
-        clearInterval(waitForControlBar)
-        appendDownloadButton(downloadButton)
-      }
-    }, 100)
+    waitForControlBarThenAppendDownloadButton(controlBar, downloadButton, liveDownloadButton)
   }
   else {
-    if (!document.body.contains(dlButton)) {
+    if (!document.body.contains(liveDownloadButton)) {
       appendDownloadButton(downloadButton)
     }
   }
 }
 
+function waitForControlBarThenAppendDownloadButton(controlBar, downloadButton, liveDownloadButton) {
+  let waitForControlBar = setInterval(() => {
+    if (document.body.contains(controlBar) && !document.body.contains(liveDownloadButton)) {
+      clearInterval(waitForControlBar)
+      appendDownloadButton(downloadButton)
+    }
+  }, 100)
+}
+
 function getDownloadButton(videoLink) {
   const downloadButton = document.createElement('a')
-  downloadButton.href = videoLink
   downloadButton.id = 'udemy-downloader-btn'
+  downloadButton.href = videoLink
   downloadButton.setAttribute('class', 'progress-display--progress-display--B20-A')
-  downloadButton.innerHTML = 'UD'
   downloadButton.setAttribute('title', 'Download Udemy Video')
+  downloadButton.innerHTML = 'UD'
 
   return downloadButton
 }
@@ -49,5 +49,5 @@ function getDownloadButton(videoLink) {
 function appendDownloadButton(downloadButton) {
   const addNoteButton = document.querySelector('[aria-label="Add note"]')
   const parentDiv = addNoteButton.parentNode
-  parentDiv.insertBefore(downloadButton, addNoteButton.nextSibling)
+  parentDiv.insertBefore(downloadButton, addNoteButton.nextSibling) //appends the download button "after/next to" the addNote button
 }
